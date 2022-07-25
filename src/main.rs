06b0f28d02167a62f6bfe1ea45244223c6e49f18
JulienDreamDev <pro.juliendacunha@gmail.com>
2022-07-25 16:02:43 +0200

    misc: gitignore updated

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,26 @@
+use std::io::{self, Write};
+
 fn hello_world() {
     println!("Hello, world!"); // "!" denote a macro
 }
 
+fn guessing_game() {
+    println!("Guess the number!");
+
+    print!("Input your guess: ");
+    io::stdout() // Flushing to ensure the output is showed before stdin
+        .flush()
+        .expect("Failed to flush stdout"); // Handle errors
+
+    let mut guess = String::new(); // "mut" denote that it's not a constant
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("Your guess is {guess}");
+}
+
 fn main() {
-    hello_world();
+    // hello_world();
+    guessing_game();
 }
