6c0129817c05370487b4a9d25b524617db604d01
JulienDreamDev <pro.juliendacunha@gmail.com>
2022-07-25 19:14:33 +0200

    variables: tupples

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -2,4 +2,8 @@ const NAME: &str = "JOHN"; // CONST are alway IMMUTABLE and can be declared in a
 
 fn main() {
     println!("Thy name is {NAME}");
+
+    let tup: (i32, char, f64) = (54, 'o', 489.36); // Tupple of int, char and double
+    let (x, y, z) = tup;
+    println!("destrucuting the tupple {x} {y} {z}");
 }
