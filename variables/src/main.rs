760dd8b4e953e9e5ea03417a8c8297f9c9f54bef
JulienDreamDev <pro.juliendacunha@gmail.com>
2022-07-25 20:03:43 +0200

    variables: functions with return values

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -6,4 +6,10 @@ fn main() {
     let tup: (i32, char, f64) = (54, 'o', 489.36); // Tupple of int, char and double
     let (x, y, z) = tup;
     println!("destructuring the tupple {x} {y} {z}");
+
+    println!("3 squared is {}", squared(3));
+}
+
+fn squared(x: i32) -> i32 { // Can be defined after the main
+    x * x // return value (without a semicolon)
 }
