3a69a7d344132093972610bab181b4a474eb0373
JulienDreamDev <pro.juliendacunha@gmail.com>
2022-07-25 16:56:18 +0200

    guessing_game: loop added

--- src/main.rs
+++ src/main.rs
@@ -11,24 +11,29 @@ fn guessing_game() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    print!("Input your guess: ");
-    io::stdout() // Flushing to ensure the output is showed before stdin
-        .flush()
-        .expect("Failed to flush stdout"); // Handle errors
+    loop {
+        print!("Input your guess: ");
+        io::stdout() // Flushing to ensure the output is showed before stdin
+            .flush()
+            .expect("Failed to flush stdout"); // Handle errors
 
-    let mut guess = String::new(); // "mut" denote that it's not a constant
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-        
-    let guess: u32 = guess.trim() // Shadowing the previous devlaration to change the type
-        .parse()
-        .expect("Please type a number !");
+        let mut guess = String::new(); // "mut" denote that it's not a constant
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+            
+        let guess: u32 = guess.trim() // Shadowing the previous devlaration to change the type
+            .parse()
+            .expect("Please type a number !");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You guessed right!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You guessed right!");
+                break; // Break the loop
+            },
+        }
     }
 }
 
